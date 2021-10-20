8bd06f1959651c582478d2c51947aea14ee6e8be
國丸裕太 <yu.kuni.maru@gmail.com>
2021-10-20 12:13:03 +0900

    実行の構成を変更

---
+++ src/WindowTest.java
@@ -0,0 +1,11 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+public class WindowTest {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
