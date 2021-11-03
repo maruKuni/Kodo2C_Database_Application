bde621f1c4699a4b521d856c13fd4b7b341b28a9
國丸裕太 <yu.kuni.maru@gmail.com>
2021-11-03 16:12:10 +0900

    本番に向けていったんコミット

---
+++ src/kodo2C/RowDataMain.java
@@ -0,0 +1,63 @@
+package kodo2C;
+import java.sql.Timestamp;
+import javafx.beans.property.*;
+public class RowDataMain {
+	//主テーブル用のデータ
+	private final SimpleStringProperty date;
+	private final SimpleStringProperty prefecture;
+	private final SimpleStringProperty centre;
+	private final SimpleStringProperty maxlv;
+	private final SimpleDoubleProperty magnitude;
+	private final SimpleDoubleProperty depth;
+	private final SimpleDoubleProperty latitude;
+	private final SimpleDoubleProperty longitude;
+	public RowDataMain(Timestamp date, String prefecture,String centre, String maxlv,double magnitude, double depth,
+			double latitude, double longitude) {
+		this.date = new SimpleStringProperty(date.toString());
+		this.prefecture = new SimpleStringProperty(prefecture);
+		this.maxlv = new SimpleStringProperty(lvToString(maxlv));
+		this.centre = new SimpleStringProperty(centre);
+		this.magnitude = new SimpleDoubleProperty(magnitude);
+		this.depth = new SimpleDoubleProperty(depth);
+		this.longitude = new SimpleDoubleProperty(longitude);
+		this.latitude = new SimpleDoubleProperty(latitude);
+	}
+	private String lvToString(String lv) {
+		if(lv.equals("5")) {
+			return "5弱";
+		}else if(lv.equals("6")) {
+			return "5強";
+		}else if(lv.equals("7")) {
+			return "6弱";
+		}else if(lv.equals("8")) {
+			return "6強";
+		}else if(lv.equals("9")) {
+			return "7";
+		}
+		return lv;
+	}
+	public StringProperty dateProperty(){
+		return date;
+	}
+	public StringProperty prefectureProperty() {
+		return prefecture;
+	}
+	public StringProperty centreProperty() {
+		return centre;
+	}
+	public StringProperty maxlvProperty() {
+		return maxlv;
+	}
+	public DoubleProperty magnitudeProperty() {
+		return magnitude;
+	}
+	public DoubleProperty depthProperty() {
+		return depth;
+	}
+	public DoubleProperty longitudeProperty() {
+		return longitude;
+	}
+	public DoubleProperty latitudeProperty() {
+		return latitude;
+	}
+}
---
+++ src/kodo2C/RowDataSub.java
@@ -0,0 +1,7 @@
+package kodo2C;
+import javafx.beans.*;
+import java.sql.Timestamp;
+import javafx.beans.property.*;
+public class RowDataSub {
+
+}
