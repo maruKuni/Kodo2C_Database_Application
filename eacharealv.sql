9ba542e19a83043bb72a380c41ecb0fc848005cf
al19113 <al19113@shibaura-it.ac.jp>
2021-11-07 15:57:56 +0900

    sqlを修正

--- eacharealv.sql
+++ eacharealv.sql
@@ -1,6 +1,8 @@
+drop table t_eacharealv;
 create table t_eacharealv(
-    quake_code char(6) primary key,
-    quake_area varchar(20) primary key,
-    quake_prefecture varchar(4),
-    lv smallint
+    quake_code char(6),
+    quake_prefecture varchar(20),
+    quake_area varchar(20),
+    lv smallint,
+    PRIMARY KEY (quake_code,quake_area)
 );
